78f1d110bbdb31dcf2f9d68649fc26aaa10406a9
Zakir <engr.zakir.bd@gmail..com>
2021-10-06 00:07:54 -0400

    signin validation test script added

---
+++ src/testngPractice/TestNGLearning.java
@@ -0,0 +1,12 @@
+package testngPractice;
+
+import org.testng.annotations.Test;
+
+public class TestNGLearning {
+	
+	@Test
+	public void firtTest() {
+		System.out.println("First Test");
+	}
+	
+}
---
+++ src/testngPractice/TestNGLearning2.java
@@ -0,0 +1,10 @@
+package testngPractice;
+
+import org.testng.annotations.Test;
+
+public class TestNGLearning2 {
+	@Test
+	public void secondTest() {
+		System.out.println("Second test");
+	}
+}
